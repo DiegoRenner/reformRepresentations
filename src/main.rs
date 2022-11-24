9d6843fb8dd1dbb5383752e95564ee4249d03542
DiegoRenner <diego.renner@hotmail.com>
2022-11-24 20:43:50 +0100

    cleaned up some naming/comments

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,54 @@
-//use number::Number;
-//extern crate rand;
 use rand::Rng;
 use rand::seq::SliceRandom;
-/// Internal ID numbers for variables.
-pub type VarName = u32;
 
 #[derive(Debug, Clone)]
-pub enum Element/*<ID: Id = VarName>*/ {
-    //VariableArgument(ID),                                        // ?a
-    //Wildcard(ID, Vec<Element<ID>>),                              // x?{...}
-    //FnWildcard(ID, Box<(Vec<Element<ID>>, Vec<Element<ID>>)>),   // f?{...}(...)
-    //Dollar(ID, Vec<Element<ID>>),                                // $x[y]
-    Var(String, usize),                                             // x^n
-    Pow(bool, Box<(Element, Element)>),                  // (1+x)^3; dirty, base, exponent
-    //NumberRange(Number, Ordering),                               // >0, <=-5/2
-    //Comparison(bool, Box<(Element<ID>, Element<ID>)>, Ordering), // x < y, x >= y, $a == 2
-    Fn(bool, String, Vec<Element>),                              // f(...)
-    Term(bool, Vec<Element>),
-    //SubExpr(bool, Vec<Element>),
+pub enum Atom{
+    Var(String, usize),
+    Pow(bool, Box<(Atom, Atom)>),
+    Fn(bool, String, Vec<Atom>),
+    Term(bool, Vec<Atom>),
     Num(bool, usize),
-    //RationalPolynomialCoefficient(bool, Box<(Polynomial, Polynomial)>),
-    //Expression(ID), // a reference to an expression
 }
 
-fn gen_init_vec(num_new_vars: usize, var_counter: &mut usize) -> Vec<Element> {
-    let mut init_vec: Vec<Element> = Vec::new();
+// generate vector of Atoms without existing Atoms
+fn gen_init_vec(num_new_vars: usize, var_counter: &mut usize) -> Vec<Atom> {
+    let mut init_vec: Vec<Atom> = Vec::new();
     for i in 0..num_new_vars {
+        // randomly choose what kind of Atom to add
         let mut rng = rand::thread_rng();
         let n1: usize = rng.gen_range(1..3);
         if n1 == 1 {
             let mut rng = rand::thread_rng();
             let n2: usize = rng.gen_range(1..10);
-            init_vec.push(Element::Num(true, n2));
+            init_vec.push(Atom::Num(true, n2));
         }
         else if n1 == 2 {
             let mut rng = rand::thread_rng();
             let n2: usize = rng.gen_range(1..10);
-            init_vec.push(Element::Var(format!("var_{}",var_counter), n2));
+            init_vec.push(Atom::Var(format!("var_{}",var_counter), n2));
             *var_counter += 1;
         }
     }
     init_vec
 }
 
-fn gen_vec(num_new_vars: usize, old_vec: Vec<Element>, var_counter: &mut usize) -> Vec<Element> {
-    let mut vec: Vec<Element> = Vec::new();
+// generate vector of Atoms based on existing Atoms
+fn gen_vec(num_new_vars: usize, old_vec: Vec<Atom>, var_counter: &mut usize) -> Vec<Atom> {
+    let mut vec: Vec<Atom> = Vec::new();
     for i in 0..num_new_vars {
+        // randomly choose what kind of Atom to add
         let mut rng = rand::thread_rng();
         let n1: usize = rng.gen_range(1..6);
         if n1 == 1 {
             let mut rng = rand::thread_rng();
             let n2: usize = rng.gen_range(1..10);
-            vec.push(Element::Num(true, n2))
+            vec.push(Atom::Num(true, n2))
 
         }
         else if n1 == 2 {
             let mut rng = rand::thread_rng();
             let n2: usize = rng.gen_range(1..10);
-            vec.push(Element::Var(format!("var_{}",var_counter), n2));
+            vec.push(Atom::Var(format!("var_{}",var_counter), n2));
             *var_counter += 1;
         }
         else if n1 == 3 {
@@ -65,27 +56,27 @@ fn gen_vec(num_new_vars: usize, old_vec: Vec<Element>, var_counter: &mut usize) 
             let size: usize = old_vec.len().try_into().unwrap();
             let n2: usize = rng.gen_range(0..size);
             let n3: usize = rng.gen_range(0..size);
-            vec.push(Element::Pow(true,Box::new((old_vec[n2].clone(),old_vec[n3].clone()))));
+            vec.push(Atom::Pow(true,Box::new((old_vec[n2].clone(),old_vec[n3].clone()))));
         }
         else if n1 == 4 {
             let mut rng = rand::thread_rng();
             let size: usize = old_vec.len().try_into().unwrap();
             let n2: usize = rng.gen_range(1..size+1);
-            vec.push(Element::Fn(true,format!("var_{}",var_counter),old_vec.choose_multiple(&mut rng, n2).cloned().collect()));
+            vec.push(Atom::Fn(true,format!("var_{}",var_counter),old_vec.choose_multiple(&mut rng, n2).cloned().collect()));
             *var_counter += 1;
         }
         else if n1 == 5 {
             let mut rng = rand::thread_rng();
             let size: usize = old_vec.len().try_into().unwrap();
             let n2: usize = rng.gen_range(1..size+1);
-            vec.push(Element::Fn(true,format!("var_{}",var_counter),old_vec.choose_multiple(&mut rng, n2).cloned().collect()));
+            vec.push(Atom::Fn(true,format!("var_{}",var_counter),old_vec.choose_multiple(&mut rng, n2).cloned().collect()));
             *var_counter += 1;
         }
         else if n1 == 6 {
             let mut rng = rand::thread_rng();
             let size: usize = old_vec.len().try_into().unwrap();
             let n2: usize = rng.gen_range(1..size+1);
-            vec.push(Element::Term(true,old_vec.choose_multiple(&mut rng, n2).cloned().collect()));
+            vec.push(Atom::Term(true,old_vec.choose_multiple(&mut rng, n2).cloned().collect()));
         }
     }
     vec
@@ -93,11 +84,17 @@ fn gen_vec(num_new_vars: usize, old_vec: Vec<Element>, var_counter: &mut usize) 
 
 fn main() {
     let mut rng = rand::thread_rng();
-    // determine number of Elements in first layer
-    let n1: usize = rng.gen_range(1..10);
+    // set parameters for generating random Atom
+    // maximum amount of elements per layer
+    max_elements = 5;
+    // maximum amount of layers
+    max_depth = 5;
+
+    // determine number of Atoms in first layer
+    let n1: usize = rng.gen_range(1..max_elements);
     
     // initialize first layer and global variable counter
-    let init_vec: Vec<Element>;
+    let init_vec: Vec<Atom>;
     let mut var_counter: usize = 0;
     init_vec = gen_init_vec(n1,&mut var_counter);
     println!("Layer 1");
@@ -105,23 +102,24 @@ fn main() {
     println!("");
 
     // build random amount of layers using elements of the previous ones
-    let mut prev_vec: Vec<Element> = init_vec;
-    let n2: usize = rng.gen_range(1..10);
+    let mut prev_vec: Vec<Atom> = init_vec;
+    let n2: usize = rng.gen_range(1..max_depth);
     for i in 0..n2 {
         // build each layer with random amount of elements
-        let n3: usize = rng.gen_range(1..10);
+        let n3: usize = rng.gen_range(1..max_elements);
         prev_vec = gen_vec(n3, prev_vec, &mut var_counter);
         println!("Layer {}", i);
         println!("{:#?}", prev_vec);
         println!("");
     }
-    // wrap everyting in an element
-    println!("Final Element");
+
+    // wrap everyting in an element to get final element
+    println!("Final Atom");
     let n4: usize = rng.gen_range(0..2);
     if n4 == 0 {
-        println!("{:#?}", Element::Fn(true,format!("var_{}",var_counter), prev_vec));
+        println!("{:#?}", Atom::Fn(true,format!("var_{}",var_counter), prev_vec));
     } else {
-        println!("{:#?}", Element::Term(true, prev_vec));
+        println!("{:#?}", Atom::Term(true, prev_vec));
     }
 
 }
